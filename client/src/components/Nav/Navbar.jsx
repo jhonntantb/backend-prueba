7fdb7c4d41db9f00ba6ba80467d05059f808ea6e
Marcelo Miranda <ticomiranda4@gmail.com>
2021-08-07 13:11:22 -0400

    Navbar deprecated

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import SearchBar from './SearchBar'
+import SignOutButton from '../Authentication/SignOut/index';
+import * as ROUTES from '../../routes';
 
 const Navbar = () => {
+
+    var authUser = sessionStorage.getItem("pg_merceria")
+
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">
+                <NavLink className="navbar-brand" to={ROUTES.LANDING}>
                     Merceria
-                </a>
+                </NavLink>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -22,20 +28,25 @@ const Navbar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <NavLink activeClassName="text-white" className="nav-link" aria-current="page" to="/">
+                        <NavLink activeClassName="text-white" className="nav-link" aria-current="page" to={ROUTES.HOME}>
                             Inicio
                         </NavLink>
+                        <SearchBar />
                         <NavLink activeClassName="text-white" className="nav-link" to="/productlist">
-                            Articulos
+                            Productos
                         </NavLink>
                         <NavLink activeClassName="text-white" className="nav-link" to="/productcreation">
                             Agregar productos
                         </NavLink>
-                        <SearchBar />
 
                     </div>
                     <div className="d-flex">
-                        <button className="btn btn-danger">Logout</button>
+                        <NavLink to={ROUTES.SIGN_IN}>
+                            <button className="btn btn-info">Sing In</button>
+                        </NavLink>
+                        <NavLink to="/">
+                            <button className="btn btn-info">Logout</button>
+                        </NavLink>
                     </div>
                 </div>
             </div>
