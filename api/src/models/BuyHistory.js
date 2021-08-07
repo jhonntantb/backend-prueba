e23b2ce5b88907a7232d9fcae92117842848435a
Federico Jakowicki <67632014+SilverPraiseTheSun@users.noreply.github.com>
2021-08-07 13:53:50 -0300

    save

--- api/src/models/BuyHistory.js
+++ api/src/models/BuyHistory.js
@@ -12,20 +12,5 @@ module.exports = (sequelize) => {
             allownull: false
 
         }
-    });
-const { DataTypes } = require("sequelize")
-
-module.exports = (sequelize) => {
-    sequelize.define("buyHistory", {
-
-        way: {
-            type: DataTypes.STRING,
-            allownull: false
-        },
-        detail: {
-            type: DataTypes.STRING,
-            allownull: false
-
-        }
-    });
+    })
 }
