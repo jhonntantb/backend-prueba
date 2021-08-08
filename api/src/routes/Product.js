1d074bf4c643fa79ef860f55ad7414e6eb122209
jexeq <jerexand@gmail.com>
2021-08-08 19:15:56 -0300

    productCreation finally working

--- api/src/routes/Product.js
+++ api/src/routes/Product.js
@@ -3,26 +3,43 @@ const {Product, Category, Productimage,Stock} = require ('../db');
 const router = require('express').Router();
 const { Op } = require("sequelize");
 
+router.post('/prueba', async function (req, res, next) {
+  var image = req.body
+  try {
+    Productimage.create({
+      productId: image.productId, 
+      image_url:image.url
+      })
+      
+  } catch (err) {next(err)};
+
+})
 
 //////////  GET PRODUCT  /////////////
 router.get("/", async function(req,res, next){
   const { name } = req.query ;
   console.log('ruta get product name: ', name);
-  try{
-    const product = await Product.findAll({include: [{ model: Category, attributes: ['id', 'name']}, {model: Productimage, attributes: ['id', 'image_url']}, {model: Stock, attributes: ['id', 'quantity', 'officeId']}]})
-     res.status(200).json(product)
-  }
-  catch (error) {next(error)};
+//   try{
+//     var product = await Product.findAll(
+//           {include: [{ model: Category, attributes: ['id', 'name']}, 
+//                       {model: Productimage, attributes: ['id', 'image_url']}, 
+//                       {model: Stock, attributes: ['id', 'quantity', 'officeId']}
+//                     ]})
 
- } )
+//      res.status(200).json(product)
+//   }
+//   catch (error) {next(error)};
 
+//  } )
 
- /*  if(!name) {
+  if(!name) {
    try{
      const product = await Product.findAll({include: [{ model: Category, attributes: ['id', 'name']}, {model: Productimage, attributes: ['id', 'image_url']}, {model: Stock, attributes: ['id', 'quantity', 'officeId']}]})
       res.status(200).json(product)
    }
-   catch (error) {next(error)};
+   catch (error) {
+     console.log('ERROR AL CREAR: ' + error.message)
+    next(error)};
   } 
   else {
     try{
@@ -30,7 +47,8 @@ router.get("/", async function(req,res, next){
        res.status(200).json(product)
     }
     catch (error) {next(error)}; 
-  } */
+  } 
+}) 
 
 
 router.get("/:idProducto", async function(req,res, next){
@@ -46,49 +64,67 @@ router.get("/:idProducto", async function(req,res, next){
 })
 
 ///////////    POST PRODUCT    ///////////
+router.post("/", async function(req, res, next){
+  try{ 
+     console.log(req.body)
+     const [product, created] =  await  Product.findOrCreate({
+                  where: {catalog_id: req.body.catalog_id},
+                  defaults: {
+                     title:req.body.title,
+                     catalog_id:req.body.catalog_id,
+                     resume:req.body.resume,
+                     detail:req.body.detail,
+                     price:req.body.price}
+                  });
+ 
+     if (!created) {  res.status(400).json("Ya existe producto con mismo nro catalogo") 
+     }
+     else {
+       req.body.category.map(async(c)=>{
+         await product.setCategories(c);
+       })
+       await Stock.create({
+           productId:product.id,
+           office_id:req.body.office_id,
+           quantity:req.body.quantity,
+           })
+ 
+           if(req.body.image.length>0){
+            //  req.body.image.forEach( async(c) =>
+            //   await Productimage.create({
+            //      productId: product.id, 
+            //      image_url:c
+            //   }) 
+            //  ) 
+            try{
+              for (let i=0; i< req.body.image.length;i++) {
+                await Productimage.create({
+                  productId: product.id,
+                  image_url: req.body.image[i]
+                })
+              }
 
-router.post("/", async function(req,res, next){
- try{ 
+            } catch(err) {next(err)}
+            
+           }
+       res.status(200).json(product) 
+     }
+ 
+  }
+ catch (error) {next(error)};
+ })
 
-    const [product, created] =  await  Product.findOrCreate({
-                 where: {catalog_id: req.body.catalog_id},
-                 defaults: {
-                    title:req.body.title,
-                    catalog_id:req.body.catalog_id,
-                    resume:req.body.resume,
-                    detail:req.body.detail,
-                    price:req.body.price}
-                 });
-
-    if (!created) {  res.status(400).json("Ya existe producto con mismo nro catalogo") 
-    }
-    else {
-      req.body.category.map(async(c)=>{
-        await product.setCategories(c);
+router.post('/prueba', async function (req, res, next) {
+  var image = req.body
+  try {
+    Productimage.create({
+      productId: image.productId, 
+      image_url:image.url
       })
-      await Stock.create({
-          productId:product.id,
-          office_id:req.body.office_id,
-          quantity:req.body.quantity,
-          })   
-          
-          if(req.body.image.length > 0){
-                req.body.image.map( c =>
-                Productimage.create({
-                    productId: product.id, 
-                    image_url:c
-                }) 
-            ) 
-        } 
 
-      res.status(200).json(product) 
-    }
+  } catch (err) {next(err)};
 
-  
- }
-catch (error) {next(error)};
 })
-
 
 
 module.exports = router;
