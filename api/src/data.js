045591de5c174bc462268d2764e800cfedc1e449
Enrique Goyret <78978458+egoyret@users.noreply.github.com>
2021-08-08 21:32:08 -0300

    productos agregagos

--- api/src/data.js
+++ api/src/data.js
@@ -29,7 +29,25 @@ const data = [
     category:[],
    
   },
-
+  {
+    title: "Alhajero Costurero Crochet    Pascuas Lana Accesorios Aguja",
+    catalog_id: 1004,
+    resume: "Marca:  comelya \nModelo:  Crochet\nFormato de venta:  Unidad\n  Unidades por pack:  1\nMaterial:  PVC\nCon luz:  No\nAccesorios incluidos:  Sí\nEs pack surtido:  Sí",
+    detail: "HERMOSO ALHAJERO /COSTURERO INPORTADO DE LA INDIA PINTADO A MANO!!",
+    price: 1700,
+    image: ["https://http2.mlstatic.com/D_Q_NP_2X_909631-MLA46779834800_072021-R.webp","https://http2.mlstatic.com/D_Q_NP_2X_909631-MLA46779834800_072021-R.webp"],
+    category:[],
+   
+  },
+  {
+     title: "Set Full Telar Xl Trapillo Crochet Manta Xl Nordica X2 Cajas",
+    catalog_id: 1005,
+    resume: "Marca:  comelya \nModelo:  Crochet\nFormato de venta:  Unidad\n  Unidades por pack:  1\nMaterial:  PVC\nCon luz:  No\nAccesorios incluidos:  Sí\nEs pack surtido:  Sí",
+    detail: "ESTE COMBO SON 2 CAJAS TAL CUAL LA FOTO OFERTA IMPERDIBLE!!",
+    price: 1700,
+    image: ["https://http2.mlstatic.com/D_Q_NP_2X_909631-MLA46779834800_072021-R.webp","https://http2.mlstatic.com/D_Q_NP_2X_909631-MLA46779834800_072021-R.webp"],
+    category:[],
+  }
 
 ];
 
