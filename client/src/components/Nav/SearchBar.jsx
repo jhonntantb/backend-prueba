946cbeb01c7abca57f710994831443a47a516aa1
Marcelo Miranda <ticomiranda4@gmail.com>
2021-08-10 19:36:38 -0400

    Funcionando!

--- client/src/components/Nav/SearchBar.jsx
+++ client/src/components/Nav/SearchBar.jsx
@@ -1,44 +1,10 @@
-import React, { useState } from 'react'
+import React,{useState} from 'react'
 import { useDispatch } from "react-redux";
-
-import { getProduct } from "../../redux/actions/product/index.js";
-import './SearchBar.css'
-
 import { useHistory} from "react-router-dom"
 import { getAllProduct } from "../../redux/actions/product/index.js";
 
 
-
 export default function SearchBar() {
-
-  const dispatch = useDispatch();
-  const BarStyling = { width: "20rem", background: "#F2F1F9", border: "none", padding: "0.5rem" };
-  const [name, setName] = useState("")
-
-  const handleInputChange = (event) => {
-    event.preventDefault();
-    setName(event.target.value);
-  };
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    dispatch(getProduct(name))
-  };
-
-  return (
-    <div className="divSearch">
-      {/* <input style={BarStyling} className="inputsearch" type="text" placeholder="Buscar... " onChange={(e) => handleInputChange(e)} /> */}
-      {/* <button className="buttonsearch" onClick={(e) => handleClick(e)}><i class="fa fa-search"></i></button> */}
-      <div className="search">
-        {" "}
-        <i class="fa fa-search" />{" "}
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Crochets, Bastidores"
-        />
-        <button className="btn">Buscar</button>
-
   const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
   const dispatch = useDispatch();
   const {push} =useHistory()
@@ -64,7 +30,5 @@ const handleClickSearch= (e)=>{
           <input style={BarStyling} className="inputsearch" type="text" placeholder="Buscar... " value={name} onChange={(e)=>handleInputSearch(e)} />
           <button style={{padding:"0.5rem",margin:"5px"}} className="buttonsearch" onClick={(e)=>handleClickSearch(e)}>Buscar</button>
       </div>
-
-    </div>
   );
 }
