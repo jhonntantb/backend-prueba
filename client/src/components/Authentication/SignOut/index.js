a69530c8000e7c8f26cf5ab444e5d2500125081e
Marcelo Miranda <ticomiranda4@gmail.com>
2021-08-10 17:28:58 -0400

    Actualizado estilo SearchBar

--- client/src/components/Authentication/SignOut/index.js
+++ client/src/components/Authentication/SignOut/index.js
@@ -34,7 +34,7 @@ function SignOutButtonBase(props) {
 
     <button type="button" onClick={clickHandler} className="btn btn-danger">
 
-      Log Out
+      Salir
     </button>
   );
 }
--- client/src/components/Nav/SearchBar.jsx
+++ client/src/components/Nav/SearchBar.jsx
@@ -1,7 +1,7 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from "react-redux";
-import { getProduct} from "../../redux/actions/product/index.js";
-
+import { getProduct } from "../../redux/actions/product/index.js";
+import './SearchBar.css'
 
 
 /*const SearchBar = () => {
@@ -26,23 +26,34 @@ export default SearchBar*/
 
 export default function SearchBar() {
   const dispatch = useDispatch();
-  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
-  const [name,setName] = useState("")
+  const BarStyling = { width: "20rem", background: "#F2F1F9", border: "none", padding: "0.5rem" };
+  const [name, setName] = useState("")
 
   const handleInputChange = (event) => {
-      event.preventDefault();
-      setName(event.target.value);
+    event.preventDefault();
+    setName(event.target.value);
   };
-  
-  const handleClick= (event) => {
-      event.preventDefault();
-      dispatch(getProduct(name))
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    dispatch(getProduct(name))
   };
 
   return (
-      <div className="divSearch">
-          <input style={BarStyling} className="inputsearch" type="text" placeholder="Buscar... " onChange={(e) => handleInputChange(e)} />
-          <button className="buttonsearch" onClick={(e) => handleClick(e)}>Buscar</button>
+    <div className="divSearch">
+      {/* <input style={BarStyling} className="inputsearch" type="text" placeholder="Buscar... " onChange={(e) => handleInputChange(e)} /> */}
+      {/* <button className="buttonsearch" onClick={(e) => handleClick(e)}><i class="fa fa-search"></i></button> */}
+      <div className="search">
+        {" "}
+        <i class="fa fa-search" />{" "}
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Crochets, Bastidores"
+        />
+        <button className="btn">Buscar</button>
       </div>
+
+    </div>
   );
 }
