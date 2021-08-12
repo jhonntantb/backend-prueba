11c987552f02ed07dc2d26d941d523f87448710c
matiascamp <campodonicomatias@hotmail.com>
2021-08-12 11:50:03 -0300

    footer responsive

--- client/src/components/Nav/SearchBar.jsx
+++ client/src/components/Nav/SearchBar.jsx
@@ -1,51 +1,42 @@
 import React, { useState } from 'react'
 import { useDispatch } from "react-redux";
-import { useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { getAllProduct } from "../../redux/actions/product/index.js";
 
 import { getProduct } from "../../redux/actions/product/index.js";
 import './SearchBar.css'
 
 export default function SearchBar() {
-  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
-  
 
-  
-  const [name,setName] = useState("")
-  
+  const [name, setName] = useState("")
+
   const dispatch = useDispatch();
-  const {push} =useHistory()
-  
+
+  const { push } = useHistory()
 
 
-  const handleInputSearch=(e)=>{
+  const handleInputSearch = (e) => {
     e.preventDefault()
     setName(e.target.value)
-}
-const handleClickSearch= (e)=>{
-    e.preventDefault();
-    if(name.trim().length>0){
-        dispatch(getAllProduct(name))
-        setName("")
-        push("/productlist")
+  }
 
+  const handleClickSearch = (e) => {
+    e.preventDefault();
+    if (name.trim().length > 0) {
+      dispatch(getAllProduct(name))
+      setName("")
+      push("/productlist")
     }
-}
+  }
 
   return (
-    <div className="divSearch">
-       <input style={BarStyling} className="form-control" type="text" placeholder="Buscar... " onChange={(e) => handleInputSearch(e)} /> 
-       <button  onClick={(e) => handleClickSearch(e)}><i class="fa fa-search"></i></button>
-    {/*  <div className="search">
-        {" "}
-        <i class="fa fa-search" />{" "}
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Crochets, Bastidores"
-        />
-        <button className="btn">Buscar</button>
-      </div>*/}
+    <div className="container">
+      <div class="row">
+      <div class="col-lg-12 col-md-3 col-sm-6">
+        <span><input className="form-control" type="text" placeholder="Buscar... " onChange={(e) => handleInputSearch(e)} /></span>
+        <span><button class="btn btn-block" onClick={(e) => handleClickSearch(e)}><i class="fa fa-search"></i></button></span>
+      </div>
+      </div>
     </div>
   )
 }
--- client/src/components/ProductList/CardProduct.jsx
+++ client/src/components/ProductList/CardProduct.jsx
@@ -3,30 +3,35 @@ import {NavLink} from "react-router-dom";
 import "./CardProduct.css"
 
 function CardProduct(props) {
+  console.log("PROPS: ",props)
     return (
- <div class="wrapper">
-    <div class="card">
-        <div class="text-center p-4"> <img id="main-image" src={props.url} width="300" /> 
-        </div>
-        <div class="thumbnail text-center"> 
-          <img onclick="change_image(this)" src={props.url} width="70"/> 
-          <img onclick="change_image(this)" src={props.url} width="70"/> 
-          <img onclick="change_image(this)" src={props.url} width="70"/> 
-        </div>
-        <div class="about text-center"> 
-              <NavLink to={`/product/${props.id}`}>
-                <a href="#" class="btn btn-primary"><h6>{props.title}</h6></a>
-                </NavLink>    
-          <span>${props.price}</span>
-        </div>
-        <div class="cart-button mt-3 px-2 d-flex justify-content-between align-items-center"> <button class="btn btn-dark text-uppercase">Add to cart</button>
-            <div class="add"> 
-              <span class="product_fav"><i class="fa fa-heart-o"></i></span> 
-              <span class="product_fav"><i class="fa fa-opencart"></i></span> 
-            </div>
-        </div>
-    </div>
- </div>
+      <div class="container-fluid ">
+      <div id="cardproduct"class="card" >
+          <div class="text-center p-4"> <img id="main-image" src={props.url} width="300" height="300" /> 
+          </div>
+          <div class="thumbnail text-center"> 
+            <img onclick="change_image(this)" src={props.url}/> 
+            <img onclick="change_image(this)" src={props.url} /> 
+            <img onclick="change_image(this)" src={props.url}/> 
+          </div>
+          <div class="about text-center"> 
+                <NavLink to={`/product/${props.id}`}>
+                  <a href="#" ><h6>{props.title}</h6></a>
+                  </NavLink>    
+            <span>${props.price}</span>
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+          <div class="cart-button"> <button class="btn btn-dark text-uppercase">Add to cart</button>
+              <div class="add"> 
+                <span class="product_fav"><i class="fa fa-heart-o"></i></span> 
+                <span class="product_fav"><i class="fa fa-info"></i></span> 
+                </div>
+                </div>
+              </div>
+          </div>
+      </div>
+   </div>
         
     )
 }
