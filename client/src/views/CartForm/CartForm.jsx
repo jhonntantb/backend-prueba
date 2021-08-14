188383a10e07e1e61b2880c6fa9bbe3764978391
Federico Jakowicki <67632014+SilverPraiseTheSun@users.noreply.github.com>
2021-08-14 15:56:56 -0300

    save

--- client/src/views/CartForm/CartForm.jsx
+++ client/src/views/CartForm/CartForm.jsx
@@ -1,51 +1,21 @@
-import { Form, Label, Input } from "reactstrap";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import AddressFrom from "../../components/ShopCart/AddressForm"
+import CartReceipt from "../../components/ShopCart/CartReceipt"
 
 export default function CartForm(){
-    const user = useSelector(state => state.userReducer.user)
-    const [provincias, setProvincias] = useState([])
-    const [localidades, setLocalidades] = useState([])
-    const [selectProv, setSelectProv] = useState("")
-    const [selectLoc, setSelectLoc] = useState("")
-
-    useEffect(() => {
-        axios.get("https://apis.datos.gob.ar/georef/api/provincias?campos=nombre&orden=nombre")
-        .then(res => setProvincias(res.data.provincias))
-        setSelectProv("Buenos Aires")
-    }, [])
-
-    useEffect(() => {
-        axios.get("https://apis.datos.gob.ar/georef/api/localidades?campos=nombre&orden=nombre&max=1000&provincia=" + selectProv)
-        .then(res => setLocalidades(res.data.localidades))
-    }, [selectProv])
-
-    const handleProvChange = (e) => {
-        setSelectProv(e.target.value)
-    }
-
+    const [address, setAddress] = useState("")
+    
     return (
-        provincias ? 
         <div>
-            <Form>
-                <Label>Provincia</Label>
-                <Input onChange={handleProvChange} type="select" name="city" id="orderCity">
-                    {provincias.map(e => <option>{e.nombre}</option>)}
-                </Input>
-                <Label>Ciudad</Label>
-                <Input type="select" name="city" id="orderCity">
-                    {localidades.map(e => <option>{e.nombre}</option>)}
-                </Input>
-                <Label>Direccion</Label>
-                <Input type="text" name="address" id="orderAddress"/>
-                <Label>Codigo Postal</Label>
-                <Input type="text" maxLength="4" name="postalCod" id="orderPostalCod"/>
-                <Label>Numero de Telefono</Label>
-                <Input type="text" maxLength="10" name="phone" id="orderPhone"/>
-            </Form>
+            {!address ? 
+            <AddressFrom setAddress={setAddress}/>
+            :
+            <div>
+                <CartReceipt/>
+                <button>Pagar</button>
+            </div>
+            }  
         </div>
-        :
-        <div>Cargando...</div>
     )
 } 
