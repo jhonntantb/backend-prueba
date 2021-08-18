135b3d13ec5e9be6acb012838a7b9a16bb86ace4
Enrique Goyret <78978458+egoyret@users.noreply.github.com>
2021-08-18 09:49:09 -0300

    Ruta de search bar / emprolija index de api / labels product creation

--- client/src/views/Product/Product.jsx
+++ client/src/views/Product/Product.jsx
@@ -62,10 +62,9 @@ export default function Product({ match }) {
 
             <div className="details col-md-6 text-center">
               <h3 className="product-title">{product.title}</h3>
-
-              <p className="product-description text-dark">{product.resume}</p>
-
               <h5 className="text-dark">{product.detail}</h5>
+              <p className="product-description text-dark">{product.resume}</p>
+          
               <h4 className="price text-dark mt-3">Articulo:{product.catalog_id}</h4>
               <h4 className="price text-dark mt-3">Stock:{product.stocks.length > 0 ? product.stocks[0].quantity :(product.stock ? product.stock : "0")} unidades</h4>
               {/* <div className="productDetails">
