10876be705c4ec88f3edb8b8bc5c21a37b4f748b
Marcelo Miranda <ticomiranda4@gmail.com>
2021-08-21 16:53:03 -0400

    Funcionando reviews con estilos y Crear Cuenta!
    Co-Authored-By: jexeq <80926059+jexeq@users.noreply.github.com>

--- client/src/components/Review/CreateReview.jsx
+++ client/src/components/Review/CreateReview.jsx
@@ -10,12 +10,12 @@ export default function CreateReview({ match }) {
   const dispatch = useDispatch();
   const id = useSelector((state) => state.userReducer.user.id);
 
-  const [values, setValues] = React.useState({
-    description: "",
-    score: 1,
-    userId: id, //ACA TENGO QUE ACCEDER AL SESSION STORAGE PARA OBTENER EL USERID
-    productId: match,
-  });
+  // const [values, setValues] = React.useState({
+  //   description: "",
+  //   score: 1,
+  //   userId: id, //ACA TENGO QUE ACCEDER AL SESSION STORAGE PARA OBTENER EL USERID
+  //   productId: match,
+  // });
   const [send, setsend] = useState("False");
   function handleSubmit(e) {
     const mostrarAlerta = () => {
@@ -25,7 +25,7 @@ export default function CreateReview({ match }) {
         text: "El campo no puede quedar vacio!",
       });
     };
-    e.preventDefault();
+    // e.preventDefault();
     if (values.description.length < 1) mostrarAlerta();
     else if (parseInt(values.score) < 1 || parseInt(values.score) > 5)
       alert("El Valor de score esta fuera del permitido");
@@ -36,27 +36,34 @@ export default function CreateReview({ match }) {
     }
   }
 
-  // const stars = Array(5).fill(0);
-  // const [currentValue, setCurrentValue] = useState(0);
-  // const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+  const [values, setValues] = useState({
+    description: "",
+    score: 0,
+    userId: id, //ACA TENGO QUE ACCEDER AL SESSION STORAGE PARA OBTENER EL USERID
+    productId: match,
+  });
+  const [hoverValue, setHoverValue] = useState(undefined);
 
-  // const colors = {
-  //   orange: "FFBA5A",
-  //   grey: "a9a9a9",
-  // };
+  const colors = {
+    orange: "FFBA5A",
+    grey: "a9a9a9",
+  };
 
-  // const handleClick = (value) => {
-  //   setCurrentValue(value);
+  const handleClick = (value) => {
+    setValues({
+      ...values,
+      score: value,
+    });
+  };
 
-  // };
+  const handleMouseOver = (value) => {
+    setHoverValue(value);
+  };
 
-  // const handleMouseOver = (value) => {
-  //   setHoverValue(value);
-  // };
-
-  // const handleMouseleave = () => {
-  //   setHoverValue(undefined);
-  // };
+  const handleMouseleave = () => {
+    setHoverValue(undefined);
+  };
 
   const alertSucces = () => {
     Swal.fire({
@@ -90,33 +97,31 @@ export default function CreateReview({ match }) {
             <label className="mt-2" htmlFor="score">
               Selecciona una puntuación{" "}
             </label>
-            {/* <div>
+            <div>
               {stars.map((_, index) => {
                 return (
                   <FaStar
                     key={index}
                     id="score"
                     size={28}
+                    value={index}
                     style={{
                       marginRight: 10,
                       cursor: "pointer",
                     }}
                     color={
-                      (hoverValue || currentValue) > index
+                      (hoverValue || values.score) > index
                         ? colors.orange
                         : colors.grey
                     }
                     onClick={() => handleClick(index + 1)}
                     onMouseOver={() => handleMouseOver(index + 1)}
                     onMouseLeave={handleMouseleave}
-                    onChange={(value) => {
-                      setValues({ ...values, score: value });
-                    }}
                   />
                 );
               })}
-            </div> */}
-            <select
+            </div>
+            {/* <select
               id="score"
               style={{ marginLeft: "5px" }}
               onChange={(e) => {
@@ -128,7 +133,7 @@ export default function CreateReview({ match }) {
               <option value="3">3</option>
               <option value="4">4</option>
               <option value="5">5</option>
-            </select>
+            </select> */}
           </div>
         </div>
         <input
--- client/src/views/Product/Product.jsx
+++ client/src/views/Product/Product.jsx
@@ -70,21 +70,7 @@ export default function Product({ match }) {
             <div className="details col-md-6 text-center">
               <h3 className="product-title">{product.title}</h3>
               <h5 className="text-dark">{product.detail}</h5>
-
-              <h4 className="price text-dark mt-3">
-                Articulo:{product.catalog_id}
-              </h4>
-              <h4 className="price text-dark mt-3">
-                Stock:
-                {product.stocks.length > 0
-                  ? product.stocks[0].quantity
-                  : product.stock
-                  ? product.stock
-                  : "0"}{" "}
-                unidades
-              </h4>
-              <h3 className="text-dark mt-3">${product.price}</h3>
-
+              <hr />
               <p className="product-description text-dark">{product.resume}</p>
 
               <h4 className="price text-dark mt-3">
@@ -99,6 +85,7 @@ export default function Product({ match }) {
                   : "0"}{" "}
                 unidades
               </h4>
+              <h3 className="text-dark mt-3">${product.price}</h3>
 
               {/* <div className="productDetails">
               </div> */}
