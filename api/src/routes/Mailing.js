73db92a6cc6fa6cd9c347d368c935b56a6a3c91a
jexeq <jerexand@gmail.com>
2021-08-22 13:36:01 -0300

    agregado envio de emails con cambio de estado de orden

--- api/src/routes/Mailing.js
+++ api/src/routes/Mailing.js
@@ -52,6 +52,8 @@ router.post("/order" , async (req, res , next) => {
     try {
       var userOk = await User.findByPk(userId)
       var orderOk = await Order.findByPk(orderId)
+
+      console.log("estado actual de la orden: - " , orderOk.status)
     } catch (err) {
       console.log("ocurrio un error buscando usuario y orden")
     }
@@ -94,12 +96,120 @@ router.post("/order" , async (req, res , next) => {
         .catch((error) => {
             
             console.log("ERROR AL ENVIAR EL MENSAJE: " , error)
-            return res.status(407).send("ERROR AL ENVIAR EL MENSAJE DE LA ORDEN: ")
+            return res.status(407).send("ERROR AL ENVIAR EL MENSAJE DE LA ORDEN: - approved")
         })
-
     }
 
+    if(orderOk.status === "shipped") {
 
+      html = `<div>
+      <h1>Estimado Usuario:</h1>
+      <p>Su pedido ha sido enviado a ${orderOk.home_address} , ${orderOk.location}</p>
+      <br/>
+      <p>Puede monitorear el estado de su pedido desde Mi Cuenta - Compras , dentro de nuestra web</p>
+      <br/>
+      <a href=http://localhost:3000/> >>>>ENLACE<<<< </a>
+      <br/>
+      <p> ¡¡¡Muchas Gracias!!! </p>
+      <br/>
+      <p>Araceli Merceria</p>
+    </div>`
+
+      msg = {
+      to: userOk.email, // Change to your recipient
+      from: 'jerexand@gmail.com', // Change to your verified sender
+      subject: 'Su pedido ha sido enviado',
+      text: 'Confirmacion de pedido enviado',
+      html
+    };
+
+      sgMail
+      .send(msg)
+      .then(() => {
+        
+        console.log("mensaje de orden despachada enviado")
+        return  res.send(`Mensaje enviado a  ${userOk.email}`)
+      })
+      .catch((error) => {
+          
+          console.log("ERROR AL ENVIAR EL MENSAJE: " , error)
+          return res.status(407).send("ERROR AL ENVIAR EL MENSAJE DE LA ORDEN: -shipped ")
+      })
+  }
+
+  if(orderOk.status === "delivered") {
+
+    html = `<div>
+    <h1>Estimado Usuario:</h1>
+    <p>Su pedido ha sido entregado a ${orderOk.home_address} , ${orderOk.location}</p>
+    <br/>
+    <p>agradeceremos que nos deje sus comentarios respecto a la experiencia de compra en nuestra web</p>
+    <br/>
+    <a href=http://localhost:3000/> >>>>ENLACE<<<< </a>
+    <br/>
+    <p> ¡¡¡Muchas Gracias!!! </p>
+    <br/>
+    <p>Araceli Merceria</p>
+  </div>`
+
+    msg = {
+    to: userOk.email, // Change to your recipient
+    from: 'jerexand@gmail.com', // Change to your verified sender
+    subject: 'Su pedido ha sido entregado',
+    text: 'Confirmacion de pedido entregado',
+    html
+  };
+
+    sgMail
+    .send(msg)
+    .then(() => {
+      
+      console.log("mensaje de orden entregada enviado")
+      return  res.send(`Mensaje enviado a  ${userOk.email}`)
+    })
+    .catch((error) => {
+        
+        console.log("ERROR AL ENVIAR EL MENSAJE: " , error)
+        return res.status(407).send("ERROR AL ENVIAR EL MENSAJE DE LA ORDEN: -delivered ")
+    })
+}
+
+if(orderOk.status === "cancelled") {
+
+  html = `<div>
+  <h1>Estimado Usuario:</h1>
+  <p>Le informamos que su orden ha sido cancelada</p>
+  <br/>
+  <p>Le esperamos nuevamente en nuestra web!</p>
+  <br/>
+  <a href=http://localhost:3000/> >>>>ENLACE<<<< </a>
+  <br/>
+  <p> ¡¡¡Muchas Gracias!!! </p>
+  <br/>
+  <p>Araceli Merceria</p>
+</div>`
+
+  msg = {
+  to: userOk.email, // Change to your recipient
+  from: 'jerexand@gmail.com', // Change to your verified sender
+  subject: 'Su pedido ha sido Cancelado',
+  text: 'Confirmacion de pedido cancelado',
+  html
+};
+
+  sgMail
+  .send(msg)
+  .then(() => {
+    
+    console.log("mensaje de orden cancelada enviado")
+    return  res.send(`Mensaje enviado a  ${userOk.email}`)
+  })
+  .catch((error) => {
+      
+      console.log("ERROR AL ENVIAR EL MENSAJE: " , error)
+      return res.status(407).send("ERROR AL ENVIAR EL MENSAJE DE LA ORDEN: -cancelled ")
+  })
+}
 
 
 })
