05b7dd44838e4e7f56cda84707f0fb3e1c47d35e
jexeq <jerexand@gmail.com>
2021-08-23 14:33:50 -0300

    se agregan categorias precargadas e imagenes a productUpdate

--- client/src/components/Authentication/CheckUser/CheckUser.jsx
+++ client/src/components/Authentication/CheckUser/CheckUser.jsx
@@ -1,6 +1,6 @@
 import {getUser} from '../../../redux/actions/user/index';
 import { useSelector, useDispatch } from 'react-redux';
-import {useState, useEffect} from 'react';
+
 
 export default function CheckUser () {
 
@@ -8,14 +8,11 @@ export default function CheckUser () {
 
     const storeUser = useSelector(state=>state.userReducer.user)
 
-    const [userLoaded, setUserLoaded] = useState(false)
+    
 
     var localUser = localStorage.getItem("pg_merceria")
     var localAdmin = localStorage.getItem("admin")
 
-    // let aux =[]
-    // aux.push(localUser)
-    // aux.push(localAdmin)
 
     if(localUser!=="guest") {
             if(storeUser.id===undefined) {
@@ -23,14 +20,7 @@ export default function CheckUser () {
             }
     }
 
-    // useEffect(()=>{
-    //     if(storeUser.id!==undefined) {
-    //         setUserLoaded(true)
-    //     }
 
-    // },[storeUser])
-
-    // if(userLoaded) return aux;
     
     return
 }
