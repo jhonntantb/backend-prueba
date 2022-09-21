2e252ae1a1d2ead3b8cd1f71bf74b28088fb476b
Jhonatan Tabraj <jhonntantb@gmail.com>
2022-09-21 15:17:55 -0500

    change a backend

--- client/src/components/Admin/Bundles/Bundles.jsx
+++
@@ -1,4 +0,0 @@
-import { useState, useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import {getAllProduct} from '../../../redux/actions/product/index';
-
--- client/src/redux/actions/checkout/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import * as TYPES from "../types"
-
-export const goToCheckout = (userId, products) => {
-    return async (dispatch) => {
-        const res = await axios.post(`/checkout/${userId}`, products)
-        return dispatch({ type: TYPES.GO_TO_CHECKOUT, payload: res.data })
-    }
-}
